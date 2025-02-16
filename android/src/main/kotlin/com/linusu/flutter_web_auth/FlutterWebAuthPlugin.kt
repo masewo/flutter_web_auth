039b7c2d80188d4be8093e84cf3c699052965b4f
masewo <masewo@gmx.net>
2025-02-16 15:08:21 +0100

    Update to Flutter Android Embedding v2

--- android/src/main/kotlin/com/linusu/flutter_web_auth/FlutterWebAuthPlugin.kt
+++ android/src/main/kotlin/com/linusu/flutter_web_auth/FlutterWebAuthPlugin.kt
@@ -7,74 +7,69 @@ import android.net.Uri
 import androidx.browser.customtabs.CustomTabsIntent
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
-import io.flutter.plugin.common.BinaryMessenger
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
 
-class FlutterWebAuthPlugin(private var context: Context? = null, private var channel: MethodChannel? = null): MethodCallHandler, FlutterPlugin {
-  companion object {
-    val callbacks = mutableMapOf<String, Result>()
-    val callbackUrls = mutableMapOf<String, String>()
+class FlutterWebAuthPlugin : FlutterPlugin, MethodCallHandler {
+    private var context: Context? = null
+    private lateinit var channel: MethodChannel
 
-    @JvmStatic
-    fun registerWith(registrar: Registrar) {
-        val plugin = FlutterWebAuthPlugin()
-        plugin.initInstance(registrar.messenger(), registrar.context())
+    companion object {
+        val callbacks = mutableMapOf<String, Result>()
+        val callbackUrls = mutableMapOf<String, String>()
     }
 
-  }
-
-  fun initInstance(messenger: BinaryMessenger, context: Context) {
-      this.context = context
-      channel = MethodChannel(messenger, "flutter_web_auth")
-      channel?.setMethodCallHandler(this)
-  }
-
-  override public fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-      initInstance(binding.getBinaryMessenger(), binding.getApplicationContext())
-  }
-
-  override public fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-      context = null
-      channel = null
-  }
-
-  override fun onMethodCall(call: MethodCall, resultCallback: Result) {
-    when (call.method) {
-        "authenticate" -> {
-          val url = Uri.parse(call.argument("url"))
-          val callbackUrlScheme = call.argument<String>("callbackUrlScheme")!!
-          val preferEphemeral = call.argument<Boolean>("preferEphemeral")!!
-
-          callbacks[callbackUrlScheme] = resultCallback
-
-          val intent = CustomTabsIntent.Builder().build()
-          val keepAliveIntent = Intent(context, KeepAliveService::class.java)
-
-          intent.intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
-          if (preferEphemeral) {
-              intent.intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
-          }
-          intent.intent.putExtra("android.support.customtabs.extra.KEEP_ALIVE", keepAliveIntent)
-
-          intent.launchUrl(context!!, url)
-        }
-        "cleanUpDanglingCalls" -> {
-          callbacks.forEach{ (_, danglingResultCallback) ->
-              danglingResultCallback.error("CANCELED", "User canceled login", null)
-          }
-          callbacks.clear()
-          resultCallback.success(null)
-        }
-        "getCallbackUrl" -> {
-          val callbackUrlScheme = call.argument<String>("callbackUrlScheme")!!
-          val url = callbackUrls.remove(callbackUrlScheme)
-          resultCallback.success(url)
-        }
-        else -> resultCallback.notImplemented()
+    override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        context = flutterPluginBinding.getApplicationContext()
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "flutter_web_auth")
+        channel.setMethodCallHandler(this)
     }
-  }
+
+    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        context = null
+        channel.setMethodCallHandler(null)
+    }
+
+    override fun onMethodCall(call: MethodCall, result: Result) {
+        when (call.method) {
+            "authenticate" -> {
+                val url = Uri.parse(call.argument("url"))
+                val callbackUrlScheme = call.argument<String>("callbackUrlScheme")!!
+                val preferEphemeral = call.argument<Boolean>("preferEphemeral")!!
+
+                callbacks[callbackUrlScheme] = result
+
+                val intent = CustomTabsIntent.Builder().build()
+                val keepAliveIntent = Intent(context, KeepAliveService::class.java)
+
+                intent.intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+                if (preferEphemeral) {
+                    intent.intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
+                }
+                intent.intent.putExtra(
+                    "android.support.customtabs.extra.KEEP_ALIVE", keepAliveIntent
+                )
+
+                intent.launchUrl(context!!, url)
+            }
+
+            "cleanUpDanglingCalls" -> {
+                callbacks.forEach { (_, danglingResultCallback) ->
+                    danglingResultCallback.error("CANCELED", "User canceled login", null)
+                }
+                callbacks.clear()
+                result.success(null)
+            }
+
+            "getCallbackUrl" -> {
+                val callbackUrlScheme = call.argument<String>("callbackUrlScheme")!!
+                val url = callbackUrls.remove(callbackUrlScheme)
+                result.success(url)
+            }
+
+            else -> result.notImplemented()
+        }
+    }
 }
